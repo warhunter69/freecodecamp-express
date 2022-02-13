b243a29489bf3464ee2c8680091dee4d97da4cfa
ahmed <warhunter69@gmail.com>
2022-02-13 20:32:19 +0200

    3

--- myApp.js
+++ myApp.js
@@ -3,8 +3,10 @@ var app = express();
 
 
 app.get('/', function (req, res) {
-    res.send("Hello Express")
-    console.log("Hello World")
+    //res.send("Hello Express")
+    //console.log("Hello World")
+    absolutePath = __dirname + "/views/index.html"
+    res.sendFile(absolutePath)
   })
 
  module.exports = app;
