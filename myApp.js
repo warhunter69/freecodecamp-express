42672e2306431d6d17f86f0e3df5df7ceff03670
ahmed <warhunter69@gmail.com>
2022-02-14 09:15:59 +0200

    4

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 
+app.use("/public", express.static(__dirname + "/public"))
+
 app.get('/', function (req, res) {
     //res.send("Hello Express")
     //console.log("Hello World")
