d1a3b6190ff4efc7b6c45e99f01e04ceec1537b5
ahmed <warhunter69@gmail.com>
2022-02-15 17:46:25 +0200

    5

--- myApp.js
+++ myApp.js
@@ -5,10 +5,11 @@ var app = express();
 app.use("/public", express.static(__dirname + "/public"))
 
 app.get('/', function (req, res) {
-    //res.send("Hello Express")
-    //console.log("Hello World")
+  
     absolutePath = __dirname + "/views/index.html"
     res.sendFile(absolutePath)
   })
-
+app.get('/json',function(req,res){
+  res.json({"message": "Hello json"})
+})
  module.exports = app;
